5fef423f932a275074b8a39099d4d55fadcb68cf
Hamilton Alvarez <alvarezhamilton@gmail.com>
2019-08-23 13:52:42 -0500

    Add tracking code

---
+++ js/tracking.js
@@ -0,0 +1,4 @@
+!function(t,e,n,s) { t.DPEventsFunction=s,t[s]=t[s] || function() { (t[s].q=t[s].q||[]).push(arguments) }; var c=e.createElement("script"),o=e.getElementsByTagName("script")[0]; c.async=1,c.src=n,o.parentNode.insertBefore(c,o); }(window, document, "https://msv-track.herokuapp.com/v1/loop.min.js", "_dp");
+
+_dp("configure", { appId: "a02f8a6f" });
+_dp("pageView");
