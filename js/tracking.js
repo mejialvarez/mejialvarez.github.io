86ac434c9fe660cbe5f6dc5ff54f8913aaeea351
Hamilton Alvarez Mejia <alvarezhamilton@gmail.com>
2019-10-09 12:55:07 -0500

    Update tracking.js

--- js/tracking.js
+++ js/tracking.js
@@ -1,4 +1,4 @@
-!function(t,e,n,s) { t.DPEventsFunction=s,t[s]=t[s] || function() { (t[s].q=t[s].q||[]).push(arguments) }; var c=e.createElement("script"),o=e.getElementsByTagName("script")[0]; c.async=1,c.src=n,o.parentNode.insertBefore(c,o); }(window, document, "https://convertloop-firehose.s3.amazonaws.com/loop.min.js", "_dp");
+!function(t,e,n,s) { t.DPEventsFunction=s,t[s]=t[s] || function() { (t[s].q=t[s].q||[]).push(arguments) }; var c=e.createElement("script"),o=e.getElementsByTagName("script")[0]; c.async=1,c.src=n,o.parentNode.insertBefore(c,o); }(window, document, "https://convertloop-files.s3.amazonaws.com/loop.min.js", "_dp");
 
 _dp("configure", { appId: "a02f8a6f" });
 _dp("pageView");
